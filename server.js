a837d43cf79aca297b35b993fb4fb91d3b5de299
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2022-03-28 19:14:37 +0700

    initial Author routes

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ if (process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index') //import exported var from index.js
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs') //set view engines to ejs
 app.set('views', __dirname + '/views') //all views in here
@@ -14,6 +16,7 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
@@ -22,5 +25,6 @@ db.on('error', error => console.error(error) )
 db.once('open', () => console.log('Connected to Mongoose') )
 
 app.use('/index', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
